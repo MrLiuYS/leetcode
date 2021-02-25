19004e18f67bddeb02156c7f97d411c9e4359c80
刘永生 <mrliuys@mrliuys.cn>
2021-02-25 13:27:03 +0800

    no message

--- dart/867.转置矩阵.dart
+++ dart/867.转置矩阵.dart
@@ -1,7 +1,9 @@
 class Solution {
   List<List<int>> transpose(List<List<int>> matrix) {
     var result = List.generate(
-        matrix[0].length, (i) => List.generate(matrix.length, (i) => 0));
+      matrix[0].length,
+      (i) => List.generate(matrix.length, (i) => 0),
+    );
 
     for (var i = 0; i < matrix.length; i++) {
       for (var j = 0; j < matrix[i].length; j++) {
