268b029cc38cd5a341f9d9dc296efce06abbf388
刘永生 <mrliuys@mrliuys.cn>
2021-09-09 11:28:38 +0800

    no message

---
+++ swift/68. 文本左右对齐❎.playground/Contents.swift
@@ -0,0 +1,13 @@
+/**
+ 68. 文本左右对齐
+ https://leetcode-cn.com/problems/text-justification/
+ */
+import UIKit
+
+class Solution {
+    func fullJustify(_ words: [String], _ maxWidth: Int) -> [String] {
+        []
+    }
+}
+
+print(Solution().fullJustify(["This", "is", "an", "example", "of", "text", "justification."], 16))
