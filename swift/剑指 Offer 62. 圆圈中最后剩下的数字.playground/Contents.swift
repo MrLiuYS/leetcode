4fcd261a2081bb29899d22242bfd3ead3129a912
刘永生 <mrliuys@mrliuys.cn>
2021-09-24 13:54:16 +0800

    剑指 Offer 62. 圆圈中最后剩下的数字  https://leetcode-cn.com/problems/yuan-quan-zhong-zui-hou-sheng-xia-de-shu-zi-lcof/

--- swift/剑指 Offer 62. 圆圈中最后剩下的数字.playground/Contents.swift
+++ swift/剑指 Offer 62. 圆圈中最后剩下的数字.playground/Contents.swift
@@ -2,6 +2,16 @@
  剑指 Offer 62. 圆圈中最后剩下的数字
  https://leetcode-cn.com/problems/yuan-quan-zhong-zui-hou-sheng-xia-de-shu-zi-lcof/
  */
-import UIKit
+class Solution {
+    func lastRemaining(_ n: Int, _ m: Int) -> Int {
+        return f(n, m)
+    }
+    func f(_ n: Int, _ m: Int) -> Int {
+        if n == 1 {
+            return 0
+        }
+        return (m + f(n-1,m)) % n
+    }
+}
 
-var greeting = "Hello, playground"
+print(Solution().lastRemaining(5, 3))
